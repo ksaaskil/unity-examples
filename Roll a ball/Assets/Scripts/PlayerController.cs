1204e79e8205323da1141aca0d977fcc80315553
Kimmo Sääskilahti <kimmo.saaskilahti@indooratlas.com>
2018-08-06 14:08:14 +0300

    First commit.

---
+++ Roll a ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+	void Start()
+	{
+        rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+        float moveHorizonal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizonal, 0, moveVertical);
+        rb.AddForce(movement * speed);
+	}
+}
